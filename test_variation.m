24d9019eff685e12454ab038c8e1507877319956
Kakdu <102024160+Kakdu@users.noreply.github.com>
2023-02-14 14:53:31 +0100

    Gjorde så att alla parametrar varieras en och en

--- test_variation.m
+++ test_variation.m
@@ -11,107 +11,105 @@ time_mesh = linspace(0,time_final,m);
 x_initial = [5*10^6; 10^3; 10^3];
 
 %% Variations in observations
-alpha = [10^-9,10^-10,10^-8,10^-10,5*10^-10];
-alpha_chosen = 4;
-big_var = 4;
-small_var = 10;
+alpha_max = [10^-7 10^-8 10^-6 10^-8 10^-7];        % rekommenderas ej
+alpha_min = [10^-11 10^-12 10^-10 10^-12 10^-12];   % rekommenderas ej
+alpha_mid = [10^-9 10^-10 10^-8 10^-10 5*10^-10];
 
 
+for i_alpha_chosen=1:5
+    % Saker att ändra för att göra andra grafer
+    alpha = alpha_max;  % Värden på parametrar som ej varieras
+    big_var = 5;        % Hur många stora linjer
+    small_var = 2;      % Hur många små linjer per stor linje
 
-if alpha_chosen == 1
-    alpha_chosen_variance = [10^-11 10^-7];
-elseif alpha_chosen == 2
-    alpha_chosen_variance = [10^-12 10^-8];
-elseif alpha_chosen == 3
-    alpha_chosen_variance = [10^-10 10^-6];
-elseif alpha_chosen == 4
-    alpha_chosen_variance = [10^-12 10^-8];
-elseif alpha_chosen == 5
-    alpha_chosen_variance = [10^-12 10^-7];
-end
-variation=logspace(log10(alpha_chosen_variance(1)),log10(alpha_chosen_variance(end)),big_var);
-variation_step=variation(2)/variation(1);
+    alpha_chosen = i_alpha_chosen;
+    alpha_chosen_variance = [alpha_min(alpha_chosen) alpha_max(alpha_chosen)];
 
-figure("Name",'Variation av parameter ...')
-clf
-for i_big_var=1:big_var
-    alpha(alpha_chosen)=variation(i_big_var);
-    alpha=alpha_vec(alpha(1),alpha(2),alpha(3),alpha(4),alpha(5),time_mesh);
-    F45 = ForwardODE45(alpha,time_mesh,x_initial);
+    variation=logspace(log10(alpha_chosen_variance(1)),log10(alpha_chosen_variance(end)),big_var);
+    variation_step=variation(2)/variation(1);
+
+    figure("Name", "Variation av parameter" + i_alpha_chosen)
+    clf
+    for i_big_var=1:big_var
+        alpha(alpha_chosen)=variation(i_big_var);
+        alpha=alpha_vec(alpha(1),alpha(2),alpha(3),alpha(4),alpha(5),time_mesh);
+        F45 = ForwardODE45(alpha,time_mesh,x_initial);
 
 
-    for i_small_var=1:small_var
-        line_width = 1/i_small_var;
-        if i_big_var == 1
-            alpha_plus_var = alpha;
-            alpha_plus_var(alpha_chosen,:) = alpha_plus_var(alpha_chosen,:)*(variation_step/2*i_small_var/small_var);
-            small_plus_F45 = ForwardODE45(alpha_plus_var,time_mesh,x_initial);
+        for i_small_var=1:small_var
+            line_width = 1/i_small_var;
+            if i_big_var == 1
+                alpha_plus_var = alpha;
+                alpha_plus_var(alpha_chosen,:) = alpha_plus_var(alpha_chosen,:)*(variation_step/2*i_small_var/small_var);
+                small_plus_F45 = ForwardODE45(alpha_plus_var,time_mesh,x_initial);
 
-            subplot(3,1,1)
-            plot(time_mesh,small_plus_F45(1,:),'--r','linewidth',line_width);
-            hold on
-            subplot(3,1,2)
-            plot(time_mesh,small_plus_F45(2,:),'--b','linewidth',line_width);
-            hold on
-            subplot(3,1,3)
-            plot(time_mesh,small_plus_F45(3,:),'--m','linewidth',line_width);
-            hold on
+                subplot(3,1,1)
+                plot(time_mesh,small_plus_F45(1,:),'--r','linewidth',line_width);
+                hold on
+                subplot(3,1,2)
+                plot(time_mesh,small_plus_F45(2,:),'--b','linewidth',line_width);
+                hold on
+                subplot(3,1,3)
+                plot(time_mesh,small_plus_F45(3,:),'--m','linewidth',line_width);
+                hold on
 
-        elseif i_big_var == length(variation)
-            alpha_minus_var=alpha;
-            alpha_minus_var(alpha_chosen,:) = alpha_minus_var(alpha_chosen,:)*(0.5 + (i_small_var-1)/small_var/2);
-            small_minus_F45 = ForwardODE45(alpha_minus_var,time_mesh,x_initial);
+            elseif i_big_var == length(variation)
+                alpha_minus_var=alpha;
+                alpha_minus_var(alpha_chosen,:) = alpha_minus_var(alpha_chosen,:)*(0.5 + (i_small_var-1)/small_var/2);
+                small_minus_F45 = ForwardODE45(alpha_minus_var,time_mesh,x_initial);
 
-            subplot(3,1,1)
-            plot(time_mesh,small_minus_F45(1,:),'--r','linewidth',line_width);
-            hold on
-            subplot(3,1,2)
-            plot(time_mesh,small_minus_F45(2,:),'--b','linewidth',line_width);
-            hold on
-            subplot(3,1,3)
-            plot(time_mesh,small_minus_F45(3,:),'--m','linewidth',line_width);
-            hold on
+                subplot(3,1,1)
+                plot(time_mesh,small_minus_F45(1,:),'--r','linewidth',line_width);
+                hold on
+                subplot(3,1,2)
+                plot(time_mesh,small_minus_F45(2,:),'--b','linewidth',line_width);
+                hold on
+                subplot(3,1,3)
+                plot(time_mesh,small_minus_F45(3,:),'--m','linewidth',line_width);
+                hold on
 
-        else
-            alpha_plus_var = alpha; alpha_minus_var=alpha;
-            alpha_plus_var(alpha_chosen,:) = alpha_plus_var(alpha_chosen,:)*(variation_step/2*i_small_var/small_var);
-            alpha_minus_var(alpha_chosen,:) = alpha_minus_var(alpha_chosen,:)*(0.5 + (i_small_var-1)/small_var/2);
-            small_plus_F45 = ForwardODE45(alpha_plus_var,time_mesh,x_initial);
-            small_minus_F45 = ForwardODE45(alpha_minus_var,time_mesh,x_initial);
+            else
+                alpha_plus_var = alpha; alpha_minus_var=alpha;
+                alpha_plus_var(alpha_chosen,:) = alpha_plus_var(alpha_chosen,:)*(variation_step/2*i_small_var/small_var);
+                alpha_minus_var(alpha_chosen,:) = alpha_minus_var(alpha_chosen,:)*(0.5 + (i_small_var-1)/small_var/2);
+                small_plus_F45 = ForwardODE45(alpha_plus_var,time_mesh,x_initial);
+                small_minus_F45 = ForwardODE45(alpha_minus_var,time_mesh,x_initial);
 
-            subplot(3,1,1)
-            plot(time_mesh,small_minus_F45(1,:),'--r',time_mesh,small_plus_F45(1,:),'--r','linewidth',line_width);
-            hold on
-            subplot(3,1,2)
-            plot(time_mesh,small_minus_F45(2,:),'--b',time_mesh,small_plus_F45(2,:),'--b','linewidth',line_width);
-            hold on
-            subplot(3,1,3)
-            plot(time_mesh,small_minus_F45(3,:),'--m',time_mesh,small_plus_F45(3,:),'--m','linewidth',line_width);
-            hold on
+                subplot(3,1,1)
+                plot(time_mesh,small_minus_F45(1,:),'--r',time_mesh,small_plus_F45(1,:),'--r','linewidth',line_width);
+                hold on
+                subplot(3,1,2)
+                plot(time_mesh,small_minus_F45(2,:),'--b',time_mesh,small_plus_F45(2,:),'--b','linewidth',line_width);
+                hold on
+                subplot(3,1,3)
+                plot(time_mesh,small_minus_F45(3,:),'--m',time_mesh,small_plus_F45(3,:),'--m','linewidth',line_width);
+                hold on
+            end
+
         end
-        
+
+
+        subplot(3,1,1)
+        plot(time_mesh,F45(1,:),'r','linewidth',1.5);
+        hold on
+        subplot(3,1,2)
+        plot(time_mesh,F45(2,:),'b','linewidth',1.5)
+        hold on
+        subplot(3,1,3)
+        plot(time_mesh,F45(3,:),'m','linewidth',1.5)
+        hold on
     end
-
-
     subplot(3,1,1)
-    plot(time_mesh,F45(1,:),'r','linewidth',1.5);
-    hold on
+    xlabel('Dagar')
+    ylabel('Tumörstorlek')
     subplot(3,1,2)
-    plot(time_mesh,F45(2,:),'b','linewidth',1.5)
-    hold on
+    xlabel('Dagar')
+    ylabel('Densitet av M1 makrofager')
     subplot(3,1,3)
-    plot(time_mesh,F45(3,:),'m','linewidth',1.5)
-    hold on
+    xlabel('Dagar')
+    ylabel('Densitet av M2 makrofager')
+
 end
-subplot(3,1,1)
-xlabel('Dagar')
-ylabel('Tumörstorlek')
-subplot(3,1,2)
-xlabel('Dagar')
-ylabel('Densitet av M1 makrofager')
-subplot(3,1,3)
-xlabel('Dagar')
-ylabel('Densitet av M2 makrofager')
 
 %% Inner functions
 
