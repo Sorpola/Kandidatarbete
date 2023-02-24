3799933314a2a205299e5241103935cb595a1940
Kakdu <102024160+Kakdu@users.noreply.github.com>
2023-02-24 11:12:11 +0100

    Fixade explicit beräkninng

--- calculate_alpha_exp.m
+++ calculate_alpha_exp.m
@@ -1,14 +1,14 @@
 function alpha_exp=calculate_alpha_exp(alpha,alpha_unknown,x,t_min,t_max)
 
-x_T=x(:,1);
-x_M1=x(:,2);
-x_M2=x(:,3);
+x_T=x(1,:);
+x_M1=x(2,:);
+x_M2=x(3,:);
 m=length(x_T);
 time_step=(t_max-t_min)/m;
-alpha_exp=zeros(m);
+alpha_exp=zeros(1,m);
 
 for k=1:m
-    x_k=x(k,:);
+    x_k=x(:,k);
     if k==1
         dxT_dt=(x_T(k+1)-x_T(k))/time_step;
         dxM1_dt=(x_M1(k+1)-x_M1(k))/time_step;
@@ -39,7 +39,6 @@ function alpha_exp_k=calc_explicit(alpha,alpha_unknown,x,dx_dt)
 dm1=alpha(1);
 dm2=alpha(2);
 at1=alpha(3);
-at2=alpha(4);
 k12=alpha(5);
 r=0.93;
 deltaM1=0.173;
@@ -50,7 +49,6 @@ betaM=9*10^8;
 xT=x(1);
 xM1=x(2);
 xM2=x(3);
-
 
 dxT_dt=dx_dt(1);
 dxM1_dt=dx_dt(2); 
@@ -65,6 +63,6 @@ elseif alpha_unknown==3
 elseif alpha_unknown==4
     alpha_exp_k=(dxM2_dt+deltaM2*xM2-k12*xM1*xT)/(xT*xM2*(1-(xM1+xM2)/betaM));
 else
-alpha_exp_k=(-dxM1_dt+at1*xT*xM1*(1-(xM1+xM2)/betaM)-deltaM1*xM1)/(xT*xM1);
+    alpha_exp_k=(-dxM1_dt+at1*xT*xM1*(1-(xM1+xM2)/betaM)-deltaM1*xM1)/(xT*xM1);
 end
 end
