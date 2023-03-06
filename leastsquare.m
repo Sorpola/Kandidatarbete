7e1bca3a07e350c15e3abb2340e233faa6729497
Jonberto <108332311+Jonberto@users.noreply.github.com>
2023-03-06 16:46:03 +0100

    Update leastsquare.m
    la till kommentarer etc

--- leastsquare.m
+++ leastsquare.m
@@ -1,68 +1,76 @@
-
+clear,clc
 % ----------------------------------------
-%   Solution of least squares  problem  min_x || Ax - y ||_2
-%   using the method of normal equations.
-%   Matrix A is constructed as a Vandermonde matrix.
-%   
+%   Solution of least squares problem  min_x || Ax - y ||_2 
 % ----------------------------------------
 
-degree=2; % graden på polynomet 
+grad=5; % graden på polynomet 
 points=200; %antalet diskreta punkter = ( rader i matrisen A)
+
+
+% x = [1 2 3 4 5];
+% y = [3 4 3.5 5 6];
 
 x=zeros(1,points);
 y=zeros(1,points);
-Vandermode_matrix=[];
+V=[];
 for i=1:1:points
   x = linspace(-10.0,10.0,points);
   %  exakt funktion
-  %y(i)= sin(pi*x(i)/5) %+ x(i)/5;
+  y(i)= sin(pi*x(i)/5); %+ x(i)/5;
   % y=rand(1,i)*0.1;
-  y(i) = 1./(1+x(i).^2) + 0.1*randn(size(x(i)));
+  %y(i) = 1./(1+x(i).^2) + 0.1*randn(size(x(i)));
  
 end
 
-%skapar vandermode matrisen 
-Vandermode_matrix = bsxfun(@power,x(:),0:degree);
 
+%skapar vandermode matrisen 
+V = bsxfun(@power,x(:),0:grad);
+%vandemorde = V 
+% V*A=Y ekvation som ska lösas
+% VL: V*V'*A dvs. VL=A
+% LÖSNINGEN V*HL "(HL=A)"
+% 
 
 % beräknar högeledet 
-rhs=Vandermode_matrix'*y';
+HL=V'*y';
 
 % beräknar vänsterledet
-lhs=Vandermode_matrix'*Vandermode_matrix;
+VL=V'*V;
 
-l=zeros(degree+1);
+%VL=zeros(degree+1);
 
 % solution of the normal equation using Cholesky decomposition
-
-if all(eig(lhs) > 0)
-    l = chol(lhs, 'lower');
-    rhs = l' \ (l \ rhs);
+%symmetri=issymmetric(VL)
+if all(eig(VL) > 0) % vi kollar om matrisen är positivt definit eftersom chol kräver "symmetrisk positiv definita matriser" annars börjar den anta 
+    VL = chol(VL, 'lower'); % chol faktoriserar matrisen lhs till en triangulär matris så att lhs=l'*l
+    %felet kan kollas med:
+    %norm(l*l' - lhs )
+    HL = VL' \ (VL \ HL); 
 
     figure(1)
-plot(x,y,'- r', 'linewidth',1)
+plot(x,y,'-- r', 'linewidth',2)
 hold on
 
 % beräknar approximation till det exakta polynomet med coefficienter c
 
-approx = Vandermode_matrix*rhs;
-plot(x,approx,'- b', 'linewidth',1)
+polynom = V*HL;
+plot(x,polynom,': b', 'linewidth',2)
 hold off
 
-str_xlabel = ['polynomgrad: ', num2str(degree)];
+str_xlabel = ['polynomgrad: ', num2str(grad)];
 
 % Beräknar det relativa felet 
 %  norm(approx. value - true value) / norm(true value)
 %e1=norm(y'- approx)/norm(y')
 
-error=norm(y'- approx)/norm(y');
+error=norm(y'- polynom)/norm(y');
 %fprintf('Relativt fel: %.2f%%', error);
 
-legend('exact ',str_xlabel);
+legend('exakt ',str_xlabel);
 title(['Relativt fel: ' num2str(error)])
 xlabel('x')
 else
-    error('Matrisen är positivt definit; en matris A sådan att x^(T) Ax > 0 för alla x');
+    error('Det uppfylls inte att matrisen är positivt definit; en matris A sådan att x^(T) Ax > 0 för alla x');
 end
 
 
