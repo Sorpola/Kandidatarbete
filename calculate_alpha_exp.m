5b132225cd92340904d9e3265f9bb526138c45ee
reginag99 <123085749+reginag99@users.noreply.github.com>
2023-03-30 14:44:48 +0200

    Update calculate_alpha_exp.m

--- calculate_alpha_exp.m
+++ calculate_alpha_exp.m
@@ -1,19 +1,13 @@
 function alpha_exp=calculate_alpha_exp(alpha,alpha_unknown,x,t_min,t_max)
 
-if alpha_unknown==1 || alpha_unknown==2 || alpha_unknown==3 || alpha_unknown==4 || alpha_unknown==5
-
-else
-    disp('---Inkompatibelt värde på variablen alpha_unknown---')
-end
-
 x_T=x(1,:);
 x_M1=x(2,:);
 x_M2=x(3,:);
-m=length(x_T);
+m=length(x_T) - 1; %ändrade till -1 då jag tycker steglängden borde bli det
 time_step=(t_max-t_min)/m;
-alpha_exp=zeros(m-2,1);
+alpha_exp=zeros(m-1,1); %ändrade från m-2 till m-1
 
-for k=2:m-1
+for k=2:m
     x_k=x(:,k);
     
         dxT_dt=(x_T(k+1)-x_T(k-1))/(2*time_step);
@@ -30,6 +24,7 @@ function alpha_exp_k=calc_explicit(alpha,alpha_unknown,x,dx_dt)
 dm1=alpha(1);
 dm2=alpha(2);
 at1=alpha(3);
+at2=alpha(4);
 k12=alpha(5);
 r=0.93;
 deltaM1=0.173;
@@ -54,6 +49,6 @@ elseif alpha_unknown==3
 elseif alpha_unknown==4
     alpha_exp_k=(dxM2_dt+deltaM2*xM2-k12*xM1*xT)/(xT*xM2*(1-(xM1+xM2)/betaM));
 else
-    alpha_exp_k=(-dxM1_dt+at1*xT*xM1*(1-(xM1+xM2)/betaM)-deltaM1*xM1)/(xT*xM1);
+    alpha_exp_k=(dxM1_dt-at1*xT*xM1*(1-(xM1+xM2)/betaM)+deltaM1*xM1)/(xT*xM1);
 end
 end
